c960482f6c062e38329444e2d507329184c75801
riricardo <ricardo.augusto.cardona@email.com>
2026-08-03 22:48:01 +0100

    chore: configure Design System

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './style/global.css'
+import App from './app/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <main>hello world</main>
+    <App />
   </StrictMode>,
 )
